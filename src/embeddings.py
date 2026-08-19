296bf1390897396edc51a666eec4d162eb508977
Vivek Makwana <111484051+V-Mak@users.noreply.github.com>
2026-08-19 23:54:23 +0530

    Add files via upload

---
+++ src/embeddings.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+def get_embedding_model():
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+        encode_kwargs={"normalize_embeddings": True}
+    )
+
+    return embeddings
